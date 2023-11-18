0f56a2aa1212558ba0f01c784eebe5647c2f6e29
SheuliAkterShathy <sheuliakter85@gmail.com>
2023-11-18 19:03:51 +0600

    done 8th module

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 // const port = 3000
 
 // parses
 app.use(express.json());
 app.use(cors());
+
+// applications routes
+app.use('/api/v1/students', StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
